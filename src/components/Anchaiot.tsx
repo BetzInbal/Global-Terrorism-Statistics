70638c80d0f70cbee681affb4d0f5d68f1eff573
betz <betzinbal@gmail.com>
2024-12-02 17:58:30 +0200

    first commit

---
+++ src/components/Anchaiot.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Anchaiot() {
+    return (
+        <div className='Anchaiot'>
+            Anchaiot
+            </div>
+    )
+}
---
+++ src/components/Heder.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Heder() {
+  return (
+    <div className='Heder'>
+    <div className='right'>
+    <img src="/assets/Badge_of_the_Israel_Defense_Forces.new.svg-D2aDM_dl.png" alt="Military Logo" className="O9j3qGhm"></img>
+
+    </div>
+        <div className='midel'>
+        <h1>מנילה צבאית</h1>
+    </div>
+        <div className='left'>
+        <a href='####'> צור קשר</a>
+        <a href='####'>תפריט מנות קרב</a>
+        <a href='####'>מה חדש</a>
+    </div>
+    </div>
+            
+  )
+}
