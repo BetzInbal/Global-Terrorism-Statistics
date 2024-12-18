ab862d92d50abb7dfcb4a6c60394bda3327e5440
betz <betzinbal@gmail.com>
2024-12-18 15:45:46 +0200

    deleted all

--- src/components/Anchaiot.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Anchaiot() {
-    return (
-        <div className='Anchaiot'>
-            Anchaiot
-            </div>
-    )
-}
--- src/components/Img.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Img() {
-    return (
-        <div className='Img'>
-        </div>
-    )
-}
