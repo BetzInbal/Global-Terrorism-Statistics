48fda35af56201afd7887b0cd8738d55113e4ebc
betz <betzinbal@gmail.com>
2024-12-19 13:51:27 +0200

    area for 2 6

---
+++ Server/src/models/AreaModel.ts
@@ -0,0 +1,63 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+interface incidents {
+    gname: string,
+    total_incidents: number
+    total_damage: number
+}
+
+const IncidentsSchema = new Schema<incidents>({
+    gname: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    total_incidents: {
+        type: Number,
+        required: true
+    },
+    total_damage: {
+        type: Number,
+        required: true
+    }
+})
+
+export interface IArea extends Document {
+    area: string,
+    incidents:incidents[],
+    total_incidents: number
+    total_damage: number,
+    latitude: number,
+    longitude: number,
+
+}
+
+const AreaSchema = new Schema<IArea>({
+    area: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    incidents: {
+        type: [IncidentsSchema],
+        default: []
+    },
+    total_incidents: {
+        type: Number,
+        required: true
+    },
+    total_damage: {
+        type: Number,
+        required: true
+    },
+    latitude: {
+        type: Number ,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    }
+    })
+    
+    export default mongoose.model<IArea>("Area", AreaSchema)
